2a47a00fe80749858b8199a64d013d92e62302bf
Armando Correa Amorelli <tato@famcorrea.com>
2025-09-29 11:58:04 -0600

    add read and write functions

--- kernel/nxp_simtemp.c
+++ kernel/nxp_simtemp.c
@@ -4,6 +4,8 @@
 #include <linux/miscdevice.h>
 #include "nxp_simtemp.h"
 
+static char text[64];
+
 static int __init my_init(void);
 static void __exit my_exit(void);
 
@@ -22,11 +24,49 @@ static int my_release (struct inode *inode, struct file *file)
     return 0;
 }
 
+static ssize_t my_read(struct file *f, char __user *user_buf, size_t len, loff_t *off)
+{
+    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len :(sizeof(text) - *off);
+    printk("nxp_simtemp - Read is called");
+
+    if (*off >= sizeof(text))
+    {
+        return 0;
+    }
+    not_copied = copy_to_user(user_buf, &text[*off], to_copy);
+    delta = to_copy - not_copied;
+    if (not_copied)
+    {
+        pr_warn("nxp_simtemp - could only copy %d bytes\n", delta);
+    }
+    *off += delta;
+    return delta;
+}
+static ssize_t my_write(struct file *f, const char __user *user_buf, size_t len, loff_t *off)
+{
+    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len :(sizeof(text) - *off);
+    printk("nxp_simtemp - Write is called");
+
+    if (*off >= sizeof(text))
+    {
+        return 0;
+    }
+    not_copied = copy_from_user(&text[*off], user_buf, to_copy);
+    delta = to_copy - not_copied;
+    if (not_copied)
+    {
+        pr_warn("nxp_simtemp - could only copy %d bytes\n", delta);
+    }
+    *off += delta;
+    return delta;
+}
 
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = my_open,
-    .release = my_release
+    .release = my_release,
+    .read = my_read,
+    .write = my_write
 };
 
 static struct miscdevice nxp_simtemp = {
