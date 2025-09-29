80ca6a51ece6189f86e916f61ecf920272ad13d0
Armando Correa Amorelli <tato@famcorrea.com>
2025-09-29 11:54:45 -0600

    add open and rlease functions

--- kernel/nxp_simtemp.c
+++ kernel/nxp_simtemp.c
@@ -7,8 +7,26 @@
 static int __init my_init(void);
 static void __exit my_exit(void);
 
+static int my_open (struct inode *inode, struct file *file)
+{
+    pr_info("nxp_simtemp - Major: %d, Minor %d\n", imajor(inode), iminor(inode));
+    pr_info("nxp_simtemp - file->f_pos: %lld\n", file->f_pos);
+    pr_info("nxp_simtemp - file->f_mode: 0x%x\n", file->f_mode);
+    pr_info("nxp_simtemp - file->f_flags: 0x%x\n", file->f_flags);
+
+    return 0;
+}
+static int my_release (struct inode *inode, struct file *file)
+{
+    pr_info("nxp_simtemp - file released");
+    return 0;
+}
+
+
 static struct file_operations fops = {
     .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_release
 };
 
 static struct miscdevice nxp_simtemp = {
