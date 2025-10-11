cf3e9eb393dc086d05b671d68d10c4f873e52c2a
Armando Correa Amorelli <tato@famcorrea.com>
2025-10-10 19:15:09 -0600

    add ring buffer, and wait queues and spinlock for safety

--- kernel/nxp_simtemp.c
+++ kernel/nxp_simtemp.c
@@ -2,12 +2,23 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/hrtimer.h>
+#include <linux/kfifo.h>
+#include <linux/wait.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/poll.h>
+
 #include "nxp_simtemp.h"
 #include "nxp_simtemp_ioctl.h"
+
 
 #define DRIVER_NAME "nxp_simtemp"
 #define DEVICE_NAME "simtemp"
 
+#define FIFO_SIZE   256 // Must be a power of two for kfifo
 
 #define SAMPLING_MS 1000
 #define TEMP_THRESHOLD 42000
@@ -20,13 +31,31 @@ static char text[64];
 static struct hrtimer timer;
 static u32 sampling_ms;
 static s32 threshold_mC;
+
+struct simtemp_device {
+
+    struct miscdevice miscdev;
+    struct hrtimer timer;
+	spinlock_t fifo_lock; /* Protects kfifo and event_flags */
+    wait_queue_head_t wq;
+
+   	/* Configuration */
+	u32 sampling_ms;
+	s32 threshold_mC;
+
+	/* The kfifo to buffer samples */
+    DECLARE_KFIFO(sample_fifo, struct simtemp_sample, FIFO_SIZE);
+};
+
+static struct simtemp_device *simtemp_device;
+
 /* --- Temperature Simulation --- */
 
 static s32 generate_temp(void)
 {
 	static s32 base_temp = 42000; /* 42.000 °C */
 	s32 temp;
-    
+
     temp = base_temp + (get_random_u32() % 200) - 100; /* +/- 0.1 °C jitter */
 
 	return temp;
@@ -34,21 +63,33 @@ static s32 generate_temp(void)
 
 static enum hrtimer_restart simtemp_timer_callback(struct hrtimer *timer)
 {
-	struct simtemp_sample sample;
+    unsigned long flags;
 
-	sample.timestamp_ns = ktime_get_ns();
+    struct simtemp_device *sdev = container_of(timer, struct simtemp_device, timer);
+    struct simtemp_sample sample;
+
+	sample.timestamp_ns = ktime_get_real_ns();
 	sample.temp_mC = generate_temp();
 	sample.flags = SIMTEMP_FLAG_NEW_SAMPLE;
 
+	spin_lock_irqsave(&sdev->fifo_lock, flags);
 
-	if (sample.temp_mC >= TEMP_THRESHOLD) {
+	if (sample.temp_mC >= sdev->threshold_mC) {
 		sample.flags |= SIMTEMP_FLAG_THRESHOLD_CROSSED;
 	}
-    
-    pr_info("nxp_simtemp - timestamp: %llu, temp: %i\n", sample.timestamp_ns, sample.temp_mC);
-	
+
+    pr_debug("nxp_simtemp - timestamp: %llu, temp: %i\n", sample.timestamp_ns, sample.temp_mC);
+
+    kfifo_put(&sdev->sample_fifo, sample);
+
+
+	spin_unlock_irqrestore(&sdev->fifo_lock, flags);
+
+	/* Wake up any processes waiting for data or events */
+	wake_up_interruptible(&sdev->wq);
+
     /* Reschedule the timer */
-	hrtimer_forward_now(timer, ms_to_ktime(sampling_ms));
+	hrtimer_forward_now(timer, ms_to_ktime(sdev->sampling_ms));
 
 	return HRTIMER_RESTART;
 }
@@ -68,50 +109,40 @@ static int my_release (struct inode *inode, struct file *file)
     pr_info("nxp_simtemp - file released");
     return 0;
 }
-static ssize_t my_read(struct file *f, char __user *user_buf, size_t len, loff_t *off)
+static ssize_t my_read(struct file *file, char __user *user_buf, size_t len, loff_t *off)
 {
+   	struct simtemp_device *sdev = file->private_data;
+    int ret;
     int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len :(sizeof(text) - *off);
     struct simtemp_sample sample;
     size_t sample_size = sizeof(struct simtemp_sample);
+   	unsigned long flags;
 
+    /* We only support reading whole samples at a time */
     if (len < sample_size)
         return -EINVAL;
 
+    /* Blocking read wait condition */
+	ret = wait_event_interruptible(sdev->wq, !kfifo_is_empty(&sdev->sample_fifo));
+	if (ret)
+		return ret;
+
     printk("nxp_simtemp - Read is called");
 
-	sample.timestamp_ns = ktime_get_real_ns();
-	sample.temp_mC = generate_temp();
-	sample.flags = SIMTEMP_FLAG_NEW_SAMPLE;
-
-    printk("nxp_simtemp - timestamp: %llu, temp: %i\n", sample.timestamp_ns, sample.temp_mC);
-
-	if (sample.temp_mC >= TEMP_THRESHOLD) {
-		sample.flags |= SIMTEMP_FLAG_THRESHOLD_CROSSED;
+    spin_lock_irqsave(&sdev->fifo_lock, flags);
+	ret = kfifo_get(&sdev->sample_fifo, &sample);
+	if (!ret) { /* Should not happen due to wait_event, but better be safe */
+		spin_unlock_irqrestore(&sdev->fifo_lock, flags);
+		return -EAGAIN;
 	}
 
-    if (*off >= sizeof(text))
+	spin_unlock_irqrestore(&sdev->fifo_lock, flags);
+
+    if (*off >= sizeof(sample))
     {
         return 0;
     }
     not_copied = copy_to_user(user_buf, &sample, sample_size);
-    delta = to_copy - not_copied;
-    if (not_copied)
-    {
-        pr_warn("nxp_simtemp - could only copy %d bytes\n", delta);
-    }
-    *off += delta;
-    return delta;
-}
-static ssize_t my_write(struct file *f, const char __user *user_buf, size_t len, loff_t *off)
-{
-    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len :(sizeof(text) - *off);
-    printk("nxp_simtemp - Write is called");
-
-    if (*off >= sizeof(text))
-    {
-        return 0;
-    }
-    not_copied = copy_from_user(&text[*off], user_buf, to_copy);
     delta = to_copy - not_copied;
     if (not_copied)
     {
@@ -151,7 +182,6 @@ static struct file_operations fops = {
     .open = my_open,
     .release = my_release,
     .read = my_read,
-    .write = my_write,
     .unlocked_ioctl = my_ioctl
 };
 
@@ -165,29 +195,43 @@ static int __init my_init(void)
 {
     sampling_ms = 1000; /* Default: 1 second */
     threshold_mC = 50000; /* Default: 50.0 °C */
-  int status;
-  printk("nxp_simtemp - Register misc device\n");
-  status = misc_register(&nxp_simtemp);
-  if (status) {
-    pr_err("nxp_simtemp - Error during Register misc device\n");
-    return -status;
-  }
-  /* Initialize and start the high-resolution timer */
-	hrtimer_init(&timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	timer.function = &simtemp_timer_callback;
-	hrtimer_start(&timer, ms_to_ktime(SAMPLING_MS), HRTIMER_MODE_REL);
+    int status;
+    simtemp_device = kzalloc(sizeof(*simtemp_device), GFP_KERNEL);
+    if (!simtemp_device){
+        return -ENOMEM;
+    }
 
-  return 0;
+    spin_lock_init(&simtemp_device->fifo_lock);
+    init_waitqueue_head(&simtemp_device->wq);
+    INIT_KFIFO(simtemp_device->sample_fifo);
 
+    simtemp_device->sampling_ms = 1000; /* Default: 1 second */
+    simtemp_device->threshold_mC = 50000; /* Default: 50.0 °C */
+
+
+    simtemp_device->miscdev = nxp_simtemp;
+
+    pr_info("nxp_simtemp - Register misc device with values: sampling: %u, threshold: %i\n", simtemp_device->sampling_ms, simtemp_device->threshold_mC);
+    status = misc_register(&simtemp_device->miscdev);
+    if (status) {
+        pr_err("nxp_simtemp - Error during Register misc device\n");
+        return -status;
+    }
+    /* Initialize and start the high-resolution timer */
+	hrtimer_init(&simtemp_device->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	simtemp_device->timer.function = &simtemp_timer_callback;
+	hrtimer_start(&simtemp_device->timer, ms_to_ktime(simtemp_device->sampling_ms), HRTIMER_MODE_REL);
+    return 0;
 }
 
 static void __exit my_exit(void)
 {
 
-  hrtimer_cancel(&timer); 
+  hrtimer_cancel(&simtemp_device->timer);
 
 
-  misc_deregister(&nxp_simtemp);
+  misc_deregister(&simtemp_device->miscdev);
+   kfree(simtemp_device);
 }
 
 module_init(my_init);
